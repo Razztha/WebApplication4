845bd56e0ead32fd46ceb0482b9ebd505dad2940
Razztha <razztha111@gmail.com>
2022-05-03 09:30:14 +0530

    initial

---
+++ Ukrain/VideoMetaData/VideoMetaData/Models/DataModelProfile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using MediaInfoDotNet;
+using MediaInfoDotNet.Models;
+
+namespace VideoMetaData.Models
+{
+    public class DataModelProfile : Profile
+    {
+        public DataModelProfile()
+        {
+            CreateMap<MediaFile, MediaFileModel>();
+            CreateMap<VideoStream, VideoInfo>();
+        }
+    }
+}
---
+++ Ukrain/VideoMetaData/VideoMetaData/Services/IMetaDataReader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MetadataExtractor;
+using VideoMetaData.Models;
+
+namespace VideoMetaData.Services
+{
+    public interface IMetaDataReader
+    {
+        Task<List<VideoInfo>> ReadVideoMetaData();
+        Task<IReadOnlyList<MetadataExtractor.Directory>> ReadVideoMetaDataLocal();
+    }
+}
